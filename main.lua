80d72c6fdf006c49f9b378fa01850fa64402f146
NullPointerException <kristopher.n.veraces@gmail.com>
2016-09-24 16:51:51 +0800

    Added race pick

--- main.lua
+++ main.lua
@@ -6,8 +6,8 @@ local storyboard = require ("storyboard")
 
 
 storyboard.purgeOnSceneChange = false
---storyboard.gotoScene("Game.Title.Title_Screen") --eto yung orig
-storyboard.gotoScene("Game.InGame.picking_a_race.pick_a_race")
+storyboard.gotoScene("Game.Title.Title_Screen") --eto yung orig
+--storyboard.gotoScene("Game.InGame.picking_a_race.pick_a_race")
 
 
 
