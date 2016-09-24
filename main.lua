7c2faf210f15bff252bf74093fbe2b077ba0085b
Kristopher <kristopher.n.veraces@gmail.com>
2016-09-25 04:59:49 +0800

    Added LevelN

--- main.lua
+++ main.lua
@@ -1,6 +1,4 @@
 display.setStatusBar( display.HiddenStatusBar )
-
-
 local storyboard = require ("storyboard")
 
 
@@ -8,7 +6,8 @@ local storyboard = require ("storyboard")
 storyboard.purgeOnSceneChange = false
 --storyboard.gotoScene("Game.Title.Title_Screen") --eto yung orig
 --storyboard.gotoScene("Game.InGame.picking_a_race.pick_a_race")
-storyboard.gotoScene("Game.InGame.main_story.level1") 
+--storyboard.gotoScene("Game.InGame.main_story.level1")
+storyboard.gotoScene("Game.InGame.main_story.levelN")
 
 
 
